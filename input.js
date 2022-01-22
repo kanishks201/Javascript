95406745a8c2d1676b41f509783657372d490adb
Kanishk Shrivas <kanishks201@gmail.com>
2022-01-21 17:41:06 -0800

    Second Commit

--- input.js
+++ input.js
@@ -2,4 +2,5 @@ const prompt = require('prompt-sync')();
 var fname = prompt("Enter First name : ");
 var lname = prompt("Enter Last name : ");
 
-console.log("Name is ",fname,lname);
+var num = prompt("Enter Number : ");
+console.log("Name is ",fname,lname,num);
